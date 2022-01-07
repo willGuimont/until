67a8fc031a72508443d8f7517336f8977a9d6008
William Guimont-Martin <wil9876543210@hotmail.com>
2022-01-06 21:12:41 -0500

    kick message

--- src/until/main.clj
+++ src/until/main.clj
@@ -31,6 +31,7 @@
                     (dopar [user user-mentions]
                            (bot/say (format "Will kick %s out of voicechat in %d minutes" (:username user) duration))
                            (Thread/sleep (* duration 60 1000))
+                           (bot/say "Sleep is for the weak but I am very weak so I need sleep")
                            (bot/say (format "Kicking %s out of voicechat" (pr-str (:username user))))
                            (http/edit-member client guild-id (:id user) :channel_id nil))
                     (bot/say "Invalid command, expected: !talk-for <duration-in-minutes> @User1 @User2 ... @UserN"))))
